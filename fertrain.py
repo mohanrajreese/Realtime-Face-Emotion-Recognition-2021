7917b1474861f0143a1de864bdcea53ec448f896
mohanrajreese <mohanrj1345@gmail.com>
2021-09-25 09:47:44 +0530

    updating on fer train for easy understanding of current running state

--- fertrain.py
+++ fertrain.py
@@ -1,7 +1,6 @@
 import sys, os
 import pandas as pd
 import numpy as np
-import numba as nba
 from sklearn.model_selection import train_test_split
 from keras.models import Sequential
 from keras.layers import Dense, Dropout, Activation, Flatten
@@ -13,6 +12,7 @@ batch_size = 64
 epochs = 100
 width, height = 48, 48
 
+
 x = np.load('./data.npy')
 y = np.load('./labels.npy')
 
@@ -20,16 +20,18 @@ x -= np.mean(x, axis=0)
 x /= np.std(x, axis=0)
 
 
-#splitting into training, validation and testing data
+print("splitting into training, validation and testing data")
 X_train, X_test, y_train, y_test = train_test_split(x, y, test_size=0.1, random_state=42)
 X_train, X_valid, y_train, y_valid = train_test_split(X_train, y_train, test_size=0.1, random_state=41)
 
-#saving the test samples to be used later
+print("saving the test samples to be used later")
 np.save('test_data', X_test)
 np.save('test_label', y_test)
 
 
-#desinging the CNN
+print("Started designing the CNN")
+print(" ")
+
 #1st layer
 model = Sequential()
 model.add(Conv2D(num_features, kernel_size=(3, 3), activation='relu', input_shape=(width, height, 1), data_format='channels_last', kernel_regularizer=l2(0.01)))
@@ -61,6 +63,9 @@ model.add(Conv2D(2*2*2*num_features, kernel_size=(3, 3), activation='relu', padd
 model.add(BatchNormalization())
 model.add(MaxPooling2D(pool_size=(2, 2), strides=(2, 2)))
 model.add(Dropout(0.5))
+print("4 layers completed")
 
 model.add(Flatten())
+print("Flattened the inputs for densing")
+
 
